1fe12b313bf1f51d7ca06268883ca73fa2c31aa0
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-10-19 16:28:25 +0200

    Kill Denis.dump()
    It's broken and have already pulled enough hairs to fix that.
    It removes so many lines it can't be wrong.

--- denis/__init__.py
+++ denis/__init__.py
@@ -1,26 +1,5 @@
 # -*- coding: utf-8 -*-
-from collections import defaultdict
-
 from django.contrib.admin.utils import NestedObjects
-from django.core.management import call_command
-from django.template import Context, Template
-from django.conf import settings
-
-import json
-import os
-
-
-DENIS_SH_TEMPLATE = """#!/bin/sh
-
-if [ -z $MANAGE ]; then
-    MANAGE={{ managepy }}
-fi
-
-{% for model in models %}
-echo PROCESSING {{ model.name }}
-$MANAGE loaddata --database {{ database }} {{ model.fixture }}
-{% endfor %}
-"""
 
 
 class Denis(object):
@@ -28,18 +7,6 @@ class Denis(object):
     def __init__(self, qs, using, **kwargs):
         self.qs = qs
         self.using = using
-
-        self.models_blacklist = kwargs.get('blacklist')
-        if not self.models_blacklist:
-            self.models_blacklist = []
-
-        # dumpdata options
-        self.indent = kwargs.get('indent')
-        if not self.indent:
-            self.indent = 2
-        self.format = kwargs.get('format')
-        if not self.format:
-            self.format = 'json'
 
     def collect(self):
         def collect_objects(obj):
@@ -79,87 +46,3 @@ class Denis(object):
             m = obj_per_model[model]
             klass = m['class']
             klass.objects.using(using).bulk_create(m['objs'])
-
-    def _parse_collected_objects(self, objects):
-        def model_name(obj):
-            return "%s.%s" % (obj._meta.app_label, obj._meta.model_name)
-
-        def get_m2m_fields(model):
-            return [
-                f.name for f in model._meta.get_fields()
-                    if (f.many_to_many and not f.auto_created)
-            ]
-
-        models = []
-        objects_ids = defaultdict(list)
-        m2m_fields = {}
-
-        for obj in objects:
-            name = model_name(obj)
-            if name not in models and name not in self.models_blacklist:
-                models.append(name)
-
-                m2ms = get_m2m_fields(obj)
-                m2m_fields[name] = m2ms
-            objects_ids[name].append(str(obj.pk))
-
-        return models, objects_ids, m2m_fields
-
-    def dump(self, outdir, database=None):
-        outdir = os.path.abspath(outdir)
-        try:
-            os.mkdir(outdir)
-        except OSError:
-            pass
-        except:
-            raise
-
-        if not database:
-            database = 'default'
-
-        def model_fixture(outdir, model, format):
-            return os.path.join(outdir, '%s.%s' % (model, self.format))
-
-        script = os.path.join(outdir, 'denis.sh')
-        with open(script, 'w+') as f:
-            flat_models = self.collect()
-            models, objects_ids, m2m_fields = self._parse_collected_objects(flat_models)
-
-            models_to_dump = []
-            for model in models:
-                pks = ','.join(objects_ids[model])
-                output = model_fixture(outdir, model, self.format)
-                if os.path.exists(output):
-                    os.unlink(output)
-
-                opts = {
-                    'primary_keys': pks,
-                    'output': output,
-                    'format': self.format,
-                    'database': self.using,
-                    'indent': self.indent,
-                    'exclude': [],
-                }
-                call_command('dumpdata', model, **opts)
-                models_to_dump.append({
-                    'name': model,
-                    'fixture': output,
-                })
-
-                # we need to remove m2m fields from models in order to avoid
-                # double insertions errors
-                if model in m2m_fields:
-                    fixture_f = open(output, 'r')
-                    fixture = json.load(fixture_f)
-                    fixture_f.close()
-
-                    with open(output, 'w') as fixture_f:
-                        for instance in fixture:
-                            for field in m2m_fields[model]:
-                                instance['fields'][field] = []
-                        json.dump(fixture, fixture_f)
-
-            manage_path = os.path.join(os.path.abspath(settings.BASE_DIR), 'manage.py')
-            template = Template(DENIS_SH_TEMPLATE)
-            ctx = Context({'models': models_to_dump, 'managepy': manage_path, 'database': database})
-            f.write(template.render(ctx))
--- test_django_denis/testdenis/tests.py
+++ test_django_denis/testdenis/tests.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.core.management import call_command
 
-from shutil import rmtree
-from subprocess import call
 import os
 
 from denis import Denis
@@ -33,36 +31,3 @@ class TestDenis(TestCase):
 
        school_isback = School.objects.using('test').filter(pk=1).exists()
        self.assertEqual(school_isback, True)
-
-    def test_dump_and_load(self):
-       appdir = os.path.abspath(os.path.dirname(__file__))
-       fixtures = os.path.join(appdir, 'fixtures', 'testdata.json')
-       auth = os.path.join(appdir, 'fixtures', 'auth.json')
-
-       # load fixtures into backup db
-       call_command('loaddata', auth, database='backup')
-       call_command('loaddata', fixtures, database='backup')
-
-       School.objects.using('test').get(pk=1).delete()
-       have_somedata = School.objects.using('test').filter(pk=1).exists()
-       self.assertEqual(have_somedata, False)
-       qs = SomeData.objects.using('backup').filter(pk=1)
-       self.assertEqual(qs.exists(), True)
-
-       outdir = os.path.join(appdir, '..', 'some-data-restore')
-       denis = Denis(qs, using='backup')
-       denis.dump(outdir=outdir, database='test')
-
-       have_dir = os.path.isdir(outdir)
-       self.assertEqual(have_dir, True)
-
-       denis_script_path = os.path.join(outdir, 'denis.sh')
-       have_script = os.path.exists(denis_script_path)
-       self.assertEqual(have_script, True)
-
-       call(['sh', denis_script_path])
-
-       school_isback = School.objects.using('test').filter(pk=1).exists()
-       self.assertEqual(school_isback, True)
-
-       rmtree(outdir)
